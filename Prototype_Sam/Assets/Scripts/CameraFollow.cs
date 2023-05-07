c5c2219f12441f2273e2ab9fd7aa1c6fe3d9e324
sam-sammie <isabella7675@gmail.com>
2023-05-07 10:02:04 -0400

    PrototypeAssignement[0.2.1.1]

---
+++ Prototype_Sam/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float CameraSpeed = 2f;
+    public Transform target;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, - 10f);
+        transform.position = newPos * CameraSpeed * Time.deltaTime;
+    }
+}
